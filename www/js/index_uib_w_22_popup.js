a6d25d4e278c9750e87ee814fe1ac53d0b7241d2
horecio <horecio@gmail.com>
2015-12-16 11:46:12 -0300

    Focus no add jogador e alter layout do topHeader

--- www/js/index_uib_w_22_popup.js
+++ www/js/index_uib_w_22_popup.js
@@ -4,25 +4,25 @@ function uib_w_22_popup_controller($scope, $ionicPopup) {
   // A confirm dialog
   $scope.show = function() {
     var confirmPopup = $ionicPopup.show({
-    template: '<input type="text" ng-model="data.wifi" placeholder="Nome do Jogador">',
-    title: 'Digite o nome do jogador.',
-    scope: $scope,
-    buttons: [
-      { text: 'Cancelar' },
-      {
-        text: '<b>Salvar</b>',
-        type: 'button-positive',
-        onTap: function(e) {
-          if (!$scope.data.wifi) {
-            //don't allow the user to close unless he enters wifi password
-            e.preventDefault();
-          } else {
-            return $scope.data.wifi;
+      template: '<input autofocus type="text" ng-model="data.wifi" placeholder="Nome do Jogador">',
+      title: 'Digite o nome do jogador.',
+      scope: $scope,
+      buttons: [
+        { text: 'Cancelar' },
+        {
+          text: '<b>Salvar</b>',
+          type: 'button-positive',
+          onTap: function(e) {
+            if (!$scope.data.wifi) {
+              //don't allow the user to close unless he enters wifi password
+              e.preventDefault();
+            } else {
+              return $scope.data.wifi;
+            }
           }
         }
-      }
-    ]
-  });
+      ]
+    });
     confirmPopup.then(function(res) {
       if(res) {
         onCreate($scope.data.wifi);
